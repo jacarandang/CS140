6b09aee73e78d4d07cf467b5c50dd04bdefc87bf
rckahi <romeocaparasiii@gmail.com>
2014-02-15 14:33:07 +0800

    Csv printing. Paki check jym.

--- Main.java
+++ Main.java
@@ -29,7 +29,10 @@ public class Main{
 	}
 	
 	public static Stat program(Unit u, boolean out)throws Exception{
+		File file = new File("output.csv");
+		PrintWriter output = new PrintWriter(new FileWriter(file));
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		Stat stat = new Stat(0,0,0,0);
 		
 		String inp;
 		
@@ -46,56 +49,69 @@ public class Main{
 		Scheduler sched;
 		if(u!=null) sched = new Scheduler(u);
 		else sched = new Scheduler(false, false, false, true, 10);
-		
-		if(out) System.out.println("Time, Stove, Ready, Assistant, Remarks");
-		int idx = 0;
-		int timer = 1;
-		while(sched.getDone() != rlist.size()){
-			if(idx < rlist.size())
-			while(rlist.get(idx).tta == timer){
-				Recipe r = rlist.get(idx);
-				r.readRecipe();
-				//System.out.println(r.name+"|");
-				//for(int i = 0; i < r.tlist.size(); i++) System.out.println(r.tlist.get(i).toString());
-				sched.arrive(r);
-				idx++;
-				if(idx >= rlist.size()) break;
+		try {	
+			//if(out) System.out.println("Time, Stove, Ready, Assistant, Remarks"); 	newLine()
+			if(out) output.write("Time, Stove, Ready, Assistant, Remarks\n");
+			
+			int idx = 0;
+			int timer = 1;
+			while(sched.getDone() != rlist.size()){
+				if(idx < rlist.size())
+				while(rlist.get(idx).tta == timer){
+					Recipe r = rlist.get(idx);
+					r.readRecipe();
+					//System.out.println(r.name+"|");
+					//for(int i = 0; i < r.tlist.size(); i++) System.out.println(r.tlist.get(i).toString());
+					sched.arrive(r);
+					idx++;
+					if(idx >= rlist.size()) break;
+				}
+				sched.update();
+				// if(out) System.out.println(sched.out(timer));
+				if(out) output.write(sched.out(timer) + "\n");
+				else sched.out(timer);
+				sched.do_();
+				timer++;
 			}
-			sched.update();
-			if(out) System.out.println(sched.out(timer));
-			else sched.out(timer);
-			sched.do_();
-			timer++;
-		}
-		
-		if(out){
-			System.out.printf("\n\n,,Time Completed, %d\n", timer);
-			System.out.printf(",,Stove Utilization, %d/%d = %.2f%%\n", sched.stove.stoveUtil, timer, (float)sched.stove.stoveUtil/(float)(timer)*100);
-		}
-		
-		int sum = 0, sum1 = 0, sum2 = 0;
-		int dishes = sched.dlist.size();
-		int totalweight = 0;
-		if(out) System.out.println(",,Job, Waiting time, Priority");
-		for(int i=0; i<sched.dlist.size(); i++){
-			Recipe temp = sched.dlist.get(i);
-			if(out) System.out.printf(",,[%s], %d, %d\n", temp.name, temp.wait, temp.priority);
-			sum += temp.wait * temp.priority;
-			sum1 += temp.wait;
-			sum2 += temp.priority;
-			totalweight += temp.priority;
-		}
-		if(out) System.out.printf(",,Total:, %d,Avg. Waiting Time %.2f, Weighted average wait time, %.2f\n", sum1, (double)sum1/(double)sched.dlist.size(), (double)sum/(double)(totalweight * dishes));
-		
-		double avg = 0;
-		for(int i = 1; i < sched.dlist.size(); i++){
-			avg += sched.dlist.get(i).priority - sched.dlist.get(i-1).priority;
-		}
-		avg/=(double)sched.dlist.size()-1;
-		if(out) System.out.printf(",,Weighted average priority difference, %.2f\n", avg);
-		Stat stat = new Stat(timer, (float)sched.stove.stoveUtil/(float)(timer), (double)sum1/(double)sched.dlist.size(), avg);
-		if(out) System.out.println(",,Fitness,"+String.format("%.2f", stat.fitness()));
+			
+			if(out){
+				// System.out.printf("\n\n,,Time Completed, %d\n", timer);
+				// System.out.printf(",,Stove Utilization, %d/%d = %.2f%%\n", sched.stove.stoveUtil, timer, (float)sched.stove.stoveUtil/(float)(timer)*100);
+				output.printf("\n\n,,Time Completed, %d\n", timer);
+				output.printf(",,Stove Utilization, %d/%d = %.2f%%\n", sched.stove.stoveUtil, timer, (float)sched.stove.stoveUtil/(float)(timer)*100);
+			}
+			
+			int sum = 0, sum1 = 0, sum2 = 0;
+			int dishes = sched.dlist.size();
+			int totalweight = 0;
+			// if(out) System.out.println(",,Job, Waiting time, Priority");
+			if(out) output.println(",,Job, Waiting time, Priority");
+			for(int i=0; i<sched.dlist.size(); i++){
+				Recipe temp = sched.dlist.get(i);
+				// if(out) System.out.printf(",,[%s], %d, %d\n", temp.name, temp.wait, temp.priority);
+				if(out) output.printf(",,[%s], %d, %d\n", temp.name, temp.wait, temp.priority);
+				sum += temp.wait * temp.priority;
+				sum1 += temp.wait;
+				sum2 += temp.priority;
+				totalweight += temp.priority;
+			}
+			// if(out) System.out.printf(",,Total:, %d,Avg. Waiting Time %.2f, Weighted average wait time, %.2f\n", sum1, (double)sum1/(double)sched.dlist.size(), (double)sum/(double)(totalweight * dishes));
+			if(out) output.printf(",,Total:, %d,Avg. Waiting Time %.2f, Weighted average wait time, %.2f\n", sum1, (double)sum1/(double)sched.dlist.size(), (double)sum/(double)(totalweight * dishes));
+			
+			double avg = 0;
+			for(int i = 1; i < sched.dlist.size(); i++){
+				avg += sched.dlist.get(i).priority - sched.dlist.get(i-1).priority;
+			}
+			avg/=(double)sched.dlist.size()-1;
+			// if(out) System.out.printf(",,Weighted average priority difference, %.2f\n", avg);
+			if(out) output.printf(",,Weighted average priority difference, %.2f\n", avg);
+			stat = new Stat(timer, (float)sched.stove.stoveUtil/(float)(timer), (double)sum1/(double)sched.dlist.size(), avg);
+			// if(out) System.out.println(",,Fitness,"+String.format("%.2f", stat.fitness()));
+			if(out) output.println(",,Fitness,"+String.format("%.2f", stat.fitness()));
+			output.close();
+		} catch(IOException e) {} 
 		return stat;
+		
 	}
 	
 }
